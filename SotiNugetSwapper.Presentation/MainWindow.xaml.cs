ea396a6dd04032e26646bd78eb93e770633609a7
Peter Hoang <peter.hoang@soti.net>
2018-01-16 10:58:21 -0500

    Added skeleton code

--- SotiNugetSwapper.Presentation/MainWindow.xaml.cs
+++ SotiNugetSwapper.Presentation/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml;
+using EnvDTE;
+using VSLangProj;
+using Window = System.Windows.Window;
 
 namespace SotiNugetSwapper.Presentation
 {
@@ -20,9 +15,93 @@ namespace SotiNugetSwapper.Presentation
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly DTE _app;
+
+        public MainWindow(DTE application)
         {
             InitializeComponent();
+
+            _app = application;
+
+            LoadReferences();
+        }
+
+        private void LoadReferences()
+        {
+            var packageDirs = new List<string>();
+            packageDirs.Add("/packages/");
+            packageDirs.Add("\\packages\\");
+
+            var solutionfile = new FileInfo(_app.Solution.FileName);
+
+            if (solutionfile.Exists)
+            {
+                var nuGetRepositoryPath = GetNuGetRepositoryPath(solutionfile.Directory);
+                if (!string.IsNullOrEmpty(nuGetRepositoryPath))
+                {
+                    packageDirs.Add(nuGetRepositoryPath);
+                }
+            }
+
+            foreach (Project project in _app.Solution.Projects.OfType<Project>())
+            {
+                var vsproject = (VSProject)project.Object;
+
+                var item1 = new TreeViewItem { Header = project.Name + " NuGet References Path" };
+                listBox.Items.Add(item1);
+
+                foreach (Reference vsReference in vsproject.References)
+                {
+                    var vsReferencePath = vsReference.Path.ToLower();
+                    foreach (string packageDir in packageDirs)
+                    {
+                        if (vsReferencePath.Contains(packageDir))
+                        {
+                            var item = new ListBoxItem
+                            {
+                                Content = vsReferencePath
+                            };
+                            listBox.Items.Add(item);
+                        }
+                    }
+                }
+            }
+        }
+
+        private string GetNuGetRepositoryPath(DirectoryInfo dir)
+        {
+            var nuGetConfigFile = dir.GetFiles("NuGet.Config").FirstOrDefault();
+
+            if (nuGetConfigFile != null)
+            {
+                var nuGetConfig = new XmlDocument();
+                nuGetConfig.Load(nuGetConfigFile.FullName);
+                var pathNode = nuGetConfig.SelectSingleNode("//config//add[contains(@key,'repositoryPath')]");
+
+                if (pathNode?.Attributes?["value"] != null)
+                {
+                    var repositoryPath = pathNode.Attributes["value"].Value;
+                    repositoryPath = Environment.ExpandEnvironmentVariables(repositoryPath);
+
+                    if (System.IO.Path.IsPathRooted(repositoryPath))
+                    {
+                        return repositoryPath;
+                    }
+
+                    var repositoryDirectory = new DirectoryInfo(System.IO.Path.Combine(nuGetConfigFile.DirectoryName, repositoryPath));
+                    if (repositoryDirectory.Exists)
+                    {
+                        return repositoryDirectory.FullName.ToLower();
+                    }
+                }
+            }
+
+            if (dir.Parent != null)
+            {
+                return GetNuGetRepositoryPath(dir.Parent);
+            }
+
+            return null;
         }
     }
 }
